9f225c98581bb87c85306c7d4b86eabc358b8855
Komalgill12 <komalpreetktwb12@gmail.com>
2025-02-13 14:05:11 -0800

    Resolved merge conflicts

--- obj/Debug/net9.0/HelloApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/HelloApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1721140f1cdb6c0c3ec06bacf7a0c1d6cc30311e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49186a2ee2b4d8d3e9ad961cc1ca88cea05d36cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
